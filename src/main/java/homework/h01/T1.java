da0fd942641ceaf47cccdf6d1dad6b15f2cba330
Жуков Максим <maksym.zhukov@e-u.edu.ua>
2026-09-22 18:17:05 +0300

    test

--- src/main/java/homework/h01/T1.java
+++ src/main/java/homework/h01/T1.java
@@ -1,5 +1,11 @@
 package homework.h01;
 
-// base
-// https://leetcode.com/problems/palindrome-number/
-public class T1 {}
+// https://leetcode.com/problems/smallest-even-multiple/
+public class T1 {
+    public int smallestEvenMultiple(int n) {
+        if (n % 2 == 0) {
+            return n;
+        }
+        return 2 * n;
+    }
+}
