b444a1481539098708f9216a4b39fc520cb19143
Maksym Zhukov <maksym.zhukov@e-u.edu.ua>
2026-09-25 18:31:50 +0300

    task

--- src/main/java/homework/h02/T1.java
+++ src/main/java/homework/h02/T1.java
@@ -1,5 +1,18 @@
 package homework.h02;
 
 // base
-// https://leetcode.com/problems/add-binary/
-public class T1 {}
+// https://leetcode.com/problems/add-digits/
+public class T1 {
+    public int addDigits(int num) {
+        while (num >= 10) {
+            int sum = 0;
+            while (num > 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+            num = sum;
+        }
+        return num;
+
+    }
+}
--- src/main/java/homework/h02/T2.java
+++ src/main/java/homework/h02/T2.java
@@ -1,5 +1,15 @@
 package homework.h02;
 
 // advanced
-// https://leetcode.com/problems/divide-two-integers/
-public class T2 {}
+// https://leetcode.com/problems/a-number-after-a-double-reversal/
+public class T2 {
+    public boolean isSameAfterReversals(int num) {
+        if (num == 0) {
+            return true;
+        }
+        if (num % 10 == 0) {
+            return false;
+        }
+        return true;
+    }
+}
