7f83c5a4e82e87b9fadc0bb40939d8d35eea3c32
Maksym Zhukov <maksym.zhukov@e-u.edu.ua>
2026-09-25 16:23:15 +0300

    LeetCode1523

--- src/main/java/homework/h01/T2.java
+++ src/main/java/homework/h01/T2.java
@@ -1,5 +1,14 @@
 package homework.h01;
 
 // advanced
-// https://leetcode.com/problems/rectangle-area/
-public class T2 {}
+//  https://leetcode.com/problems/count-odd-numbers-in-an-interval-range/
+public class T2 {
+    class Solution {
+        public int countOdds(int low, int high) {
+            if (low % 2 == 0 && high % 2 == 0)
+                return (high - low) / 2;
+            else
+                return ((high - low) / 2) + 1;
+        }
+    }
+}
